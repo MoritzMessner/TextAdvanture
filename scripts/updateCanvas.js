378faff17f835aa1d2109e0b9c3fad0458cc54e8
MessnerM <44976678+MessnerM@users.noreply.github.com>
2020-07-12 20:17:56 +0200

    update to caching user inputs

--- scripts/updateCanvas.js
+++ scripts/updateCanvas.js
@@ -1,39 +1,45 @@
-window.onload = async function() {
-        var el = document.getElementsByClassName("felx-item")[0];
-        el.classList.add('show');
-        el.classList.remove('hide');
+var lastCommands = [];
+var inputElement;
+window.onload = function () {
+    var el = document.getElementsByClassName("felx-item")[0];
+    el.classList.add('show');
+    el.classList.remove('hide');
+    inputElement = document.getElementById("playerConsole");
+    inputElement.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            var userMessage = inputElement.value;
+            var outputDiv = document.getElementById("output");
+            lastCommands.push(userMessage);
+            inputElement.value = "";
+            outputDiv.innerHTML += "<p>> " + userMessage + "</p>";
+            cacheControll(inputElement);
+        }
+        //ScrollBar nach immer nach ganz unten
+        var objDiv = document.getElementById("output");
+        objDiv.scrollTop = objDiv.scrollHeight;
 
+    });
+}
 
-        var input = document.getElementById("playerConsole");
-        input.addEventListener("keyup", function(event) {
-            if (event.keyCode === 13) {
-                var inputElement = document.getElementById("playerConsole");
-                var userMessage = inputElement.value;
-                var outputDiv = document.getElementById("output");
-
-                inputElement.value = "";
-
-                outputDiv.innerHTML += "<p>> " + userMessage + "</p>";
-                /*var spllitInput = userMessage.split("");
-
-                for (var i = 0; i < spllitInput.length; i++) {
-                    outputDiv.innerHTML += spllitInput[i];
-                    await sleep(i * 10000);
+function cacheControll(inputElement) {
+    var index = 0;
+    document.onkeydown = function (e) {
+        switch (e.keyCode) {
+            case 38:
+                if (index < lastCommands.length) {
+                    inputElement.value = lastCommands[lastCommands.length - index - 1];
+                    if (index !== lastCommands.length - 1)
+                        index++;
                 }
-                outputDiv.innerHTML += "</p>";
-                */
-            }
+                break;
+            case 40:
+                if (index > 0) {
+                    index--;
+                    inputElement.value = lastCommands[lastCommands.length - index - 1];
+                }
+                break;
 
+        }
 
-            //ScrollBar nach immer nach ganz unten
-            var objDiv = document.getElementById("output");
-            objDiv.scrollTop = objDiv.scrollHeight;
-
-        });
-
-    }
-    /*
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    */
+    };
+}
